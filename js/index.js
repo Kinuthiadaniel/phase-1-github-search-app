7a97d76abb659e0bdf8dba582b5d1ee8d3ce3868
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-04-12 09:42:13 +0300

    changes

---
+++ js/index.js
@@ -0,0 +1,53 @@
+const form = document.querySelector('#github-form');
+
+document.addEventListener('DOMContentLoaded', () => {
+  form.addEventListener('submit', searchGitUser);
+  console.log(searchGitUser);
+});
+
+//function to Get user information
+let searchGitUser = (e)=>{
+e.preventDefault();
+const user= document.querySelector("#search").value;
+fetch(`https://api.github.com/search/users?q=${user}`)
+.then(resp => resp.json())
+.then(users=>{
+  renderUserDetails(users.items)
+})
+.catch((error)=>{
+  alert(error.message)
+})
+}
+
+//function to render user details
+let renderUserDetails = (users)=>{
+  users.forEach(user=>{
+    createGitUserElement(user)
+  })
+}
+
+//function to create git user card element
+
+let createGitUserElement = (user)=>{
+  const gitUserCard = document.createElement('div');
+  gitUserCard.className = 'user-card';
+  document.querySelector('#user-list').appendChild(gitUserCard);
+  gitUserCard.innerHTML = `<img src="${user.avatar_url}" >
+    <h2>${user.login}</h2>
+    <a class="fs11" href="${user.html_url}" target="_blank">Go to Github Profile</a><br>
+    <button class="submit-btn fs11">View ${user.login}'s Repos</button>
+    `
+    gitUserCard.querySelector(".submit-btn").addEventListener('click', () => {
+      fetchRepo(user)
+    })
+};
+
+//function to fetch repositories
+let fetchRepo=(user)=>{
+  fetch(`https://api.github.com/users/${user.login}/repos`)
+ .then(resp => resp.json())
+ .then(repos => {
+    renderRepositoryDetails(repos)
+  })
+  .catch(error => { alert(error.message) });
+}
