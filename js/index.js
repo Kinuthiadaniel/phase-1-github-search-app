0744023fc6324f059db279be6665972aa67bee97
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-04-12 09:45:58 +0300

    changes

--- js/index.js
+++ js/index.js
@@ -51,3 +51,24 @@ let fetchRepo=(user)=>{
   })
   .catch(error => { alert(error.message) });
 }
+
+//function to render repository details
+let renderRepositoryDetails = (repos)=>{
+  repos.forEach(repo=>{
+    createGitRepoElement(repo)
+  })
+}
+
+//function to create git repo card element
+let createGitRepoElement = (repo)=>{
+  const gitRepoCard = document.createElement('div');
+  gitRepoCard.className = 'repo-card';
+  document.querySelector('#repo-list').appendChild(gitRepoCard);
+  gitRepoCard.innerHTML = `<img src="${repo.owner.avatar_url}" >
+    <h2>${repo.name}</h2>
+    <a class="fs11" href="${repo.html_url}" target="_blank">Go to Github Repository</a><br>
+    <button class="submit-btn fs11">View ${repo.name}'s Issues</button>
+    `
+    
+};
+
